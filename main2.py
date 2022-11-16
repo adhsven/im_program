eeda44bd1f7162f852a2289c1c4feac40175fbff
adhsven <sven.aufderheide@stud.uni-hannover.de>
2022-11-16 18:57:41 +0100

    csv über implementierte Funktion in pandas, noch zu reinigen

--- main2.py
+++ main2.py
@@ -1,10 +1,12 @@
 import csv
-
+import pandas as pd
 from requests_html import HTMLSession
 
-s = HTMLSession()
+
+#s = HTMLSession()
 
 def get_post_links(page):
+    s = HTMLSession()
     url = f'https://www.photovoltaikforum.com/board/10-versicherungen/?pageNo={page}'
     linklist=[]
     r=s.get(url)
@@ -14,33 +16,43 @@ def get_post_links(page):
         linklist.append(post.find('a',first=True).attrs['href'])
     return linklist
 
-def get_title_and_content(testlink):
+def get_title(testlink):
+    s = HTMLSession()
     r = s.get(testlink)
-    title= r.html.find('h1.contentTitle',first=True).text.strip()
+    title = r.html.find('h1.contentTitle',first=True).text.strip()
+    return title
+
+def get_content(testlink):
+    s = HTMLSession()
+    r = s.get(testlink)
     issues_and_answers = r.html.find('div.messageText p')
     content = []
     for issue in issues_and_answers:
         content.append(issue.text.strip())
-    topic = {
-        'title': title,
-        'conversation': content
-    }
-    return topic
-
-def save_csv(topics):
-    keys = topics[0].keys()
-
-    with open('forum.csv','w') as f:
-        dict_writer = csv.DictWriter(f,keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(topics)
+    return content
 
 
 
-for idx_page in range(1,3):
-    links = get_post_links(idx_page)
-    topics = []
-    for link in links:
-        topics.append(get_title_and_content(link))
-    #save_csv(topics)
-    print(topics)
+#main
+df = pd.DataFrame(
+    columns=['URL','Topic','Content']
+)
+for idx_page in range(1,41):
+    print(f'scraping page {idx_page}...')
+    linklist_page = get_post_links(idx_page)
+    for idx_link in linklist_page:
+        #current_url = linklist_page(idx_link)
+        current_title = get_title(idx_link)
+        current_content = get_content(idx_link)
+
+        dict = {'URL': idx_link,
+                'Topic': current_title,
+                'Content': current_content}
+        df = df.append(dict, ignore_index=True)
+
+df.to_csv('firsttry.csv',index=False)
+
+
+
+
+
